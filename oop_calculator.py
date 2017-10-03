7dce83b3b3cef8acc58399b0881e0ada36bd147a
laurauzcategui <laura.uzcateguij@gmail.com>
2017-10-03 21:16:26 +0100

    Adding OOP Calculator

---
+++ oop_calculator.py
@@ -0,0 +1,24 @@
+class Calculator(object):
+
+    def __init__(self):
+        self.operations = {
+            '+' : lambda x,y : x + y,
+            '-' : lambda x,y : x - y,
+            '*' : lambda x,y : x * y,
+            '/' : lambda x,y : x / y
+        }
+
+    def operation(self,a,b,operate):
+        return self.operations[operate](a,b)
+
+    def add(self,a,b):
+        return self.operation(a,b,'+')
+    def subtract(self,a,b):
+        return self.operation(a,b,'-')
+    def multiply(self,a,b):
+        return self.operation(a,b,'*')
+    def divide(self,a,b):
+        try:
+            return self.operation(a,b,'/')
+        except ZeroDivisionError:
+            print('Error, You cannot divide by Zero')
